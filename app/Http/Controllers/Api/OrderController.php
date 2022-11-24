7a45440acc5d7b9a80d703df12e8bb4779159485
Bimapr_1701 <19523151@students.uii.ac.id>
2022-11-24 16:44:25 +0700

    Update Order

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -207,4 +207,249 @@ class OrderController extends Controller
             'code'    => 406
         ], 406);
     }
+
+    public function getOrderByID(Request $request, $find)
+    {
+        $order = Order::where('id_order', $find)->first();
+        
+        if($order) {
+            return response()->json([
+                'success' => true,
+                'data'    => [
+                    'order'      => $order
+                ],
+                'message' => 'Berhasil mendapatkan data pesanan!',
+                'code'    => 201
+            ], 201);
+        }
+        
+        return response()->json([
+            'success' => false,
+            'data'    => '',
+            'message' => 'Order ID tidak ditemukan, silahkan cek kembali!',
+            'code'    => 406
+        ], 406);
+    }
+
+    public function updateOrder(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'type'         => 'required|in:SMS,Whatsapp,Voice',
+        ],[
+            'type.required'=> 'Service tidak boleh kosong!',
+            'type.in'      => 'Service hanya tersedia SMS, Whatsapp, Voice',
+        ]);
+
+        //if validation fails
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'data'    => $validator->errors(),
+                'message' => 'Gagal melakukan pemesanan!',
+                'code'    => 422
+            ], 422);
+        }
+
+        $service = Service::where('name', $request->type)->first();
+        $datetime = Carbon::now();
+        $date = $datetime->format('Y-m-d');
+        $time = $datetime->format('H:i:s');
+        
+        $validator = Validator::make($request->all(), [
+            'order_id'     => 'required|numeric',
+            'phone'        => 'required|numeric|digits_between:10,14',
+            'send_date'    => 'required|after_or_equal:' . $date,
+            'message'      => 'required|max:' . $service->max_char,
+            'send_time'    => 'required',
+        ],[
+            'phone.required'            => 'Nomer telepon tidak boleh kosong!',
+            'phone.numeric'             => 'Nomer telepon yang anda masukkan bukan angka!',
+            'phone.digits_between'      => 'Nomer yang dimasukkan kurang dari 10 angka atau lebih dari 13!',
+            'send_date.required'        => 'Tanggal pengiriman tidak boleh kosong!',
+            'send_date.after_or_equal'  => 'Tanggal yang dimasukkan tidak dapat sebelum tanggal ' . $date,
+            'message.required'          => 'Jumlah huruf yang dimasukkan melebihi batas maksimal!',
+            'message.max'               => 'Jumlah huruf yang dimasukkan melebihi batas maksimal!',
+            'send_time.required'        => 'Waktu pengiriman tidak boleh kosong!',
+        ]);
+
+        //if validation fails
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'data'    => $validator->errors(),
+                'message' => 'Gagal melakukan pemesanan!',
+                'code'    => 422
+            ], 422);
+        }
+
+        $layanan = 'send' . $service->name . 'Image';
+        if($request->image_link == null) {
+            $request->image_link = '';
+            $layanan = 'send' . $service->name;
+        }
+        if($request->caption == null) {
+            $request->caption = '';
+        }
+        $price = $service->price;
+
+        $updateOrder = Order::where('id_order', $request->order_id)->first();
+
+        $users = User::find($request->user()->id_user)->first();
+        $users->balance = $request->user()->balance + $updateOrder->price;
+        $users->save();
+        
+        if($request->user()->balance < $price) {
+            return response()->json([
+                'success' => false,
+                'data'    => '',
+                'message' => 'Saldo anda tidak cukup!',
+                'code'    => 406
+            ], 406);
+        }
+
+        if($request->send_date == $date && $request->send_time <= $time) {
+            $order = UpdateController::$layanan($request->phone, $request->message, $request->image_link, $request->caption);
+            
+            if($order['messageId'] == null) {
+                return response()->json([
+                    'success' => false,
+                    'data'    => '',
+                    'message' => $order['text'],
+                    'code'    => 409
+                ], 409);
+            }
+
+            $status = UpdateController::getStatus($service->type, $order['messageId']);
+            
+            //return JSON process update failed 
+            if($order) {
+                $users = User::find($request->user()->id_user)->first();
+                $users->balance = $request->user()->balance - $price;
+                $users->save();
+                
+                $updateOrder->phone         = $request->phone;
+                $updateOrder->message       = $request->message;
+                $updateOrder->send_date     = $request->send_date;
+                $updateOrder->send_time     = $request->send_time;
+                if($request->image_link != null) {
+                    $updateOrder->image     = $request->image_link;
+                }
+                if($request->caption != null) {
+                    $updateOrder->caption       = $request->caption;
+                }
+                $updateOrder->price         = $price;
+                if($status["msg-status"] != null) {
+                    $updateOrder->status        = $status["msg-status"];
+                } else {
+                    $updateOrder->status        = 'No Status';
+                }
+                $updateOrder->type          = $service->id_service;
+                $updateOrder->message_id    = $order['messageId'];
+                $updateOrder->save();
+
+                if($updateOrder) {
+                    //return response JSON user password is updated
+                    return response()->json([
+                        'success' => true,
+                        'data'    => [
+                            'order' => $updateOrder
+                        ],
+                        'message' => 'Berhasil mengubah pesanan!',
+                        'code'    => 200
+                    ], 200);
+                }
+            }
+        } else {
+            $users = User::find($request->user()->id_user)->first();
+            $users->balance = $request->user()->balance - $price;
+            $users->save();
+            
+            $updateOrder->phone         = $request->phone;
+            $updateOrder->message       = $request->message;
+            $updateOrder->send_date     = $request->send_date;
+            $updateOrder->send_time     = $request->send_time;
+            if($request->image_link != null) {
+                $updateOrder->image     = $request->image_link;
+            }
+            if($request->caption != null) {
+                $updateOrder->caption       = $request->caption;
+            }
+            $updateOrder->price         = $price;
+            $updateOrder->type          = $service->id_service;
+            $updateOrder->save();
+    
+            if($updateOrder) {
+                //return response JSON when success
+                return response()->json([
+                    'success' => true,
+                    'data'    => [
+                        'order' => $updateOrder
+                    ],
+                    'message' => 'Berhasil mengubah pesanan!',
+                    'code'    => 200
+                ], 200);
+            }
+        }
+
+        //return JSON process insert failed 
+        return response()->json([
+            'success' => false,
+            'user'    => '',
+            'message' => 'Pembaruan data gagal dilakukan!',
+            'code'    => 409
+        ], 409);
+    }
+
+    public function cancelOrder(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'order_id'        => 'required|numeric',
+        ],[
+            'order_id.required'        => 'Order ID tidak boleh kosong!',
+            'order_id.numeric'         => 'Order ID yang dimasukkan bukan angka!',
+        ]);
+
+        //if validation fails
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'data'    => $validator->errors(),
+                'message' => 'Gagal membatalkan Pesanan!',
+                'code'    => 422
+            ], 422);
+        }
+
+        $orderCanceled = Order::where('users_id', $request->user()->id_user)->where('id_order', $request->order_id)->first();
+        
+        //if validation fails
+        if(!$orderCanceled) {
+            return response()->json([
+                'success' => false,
+                'data'    => '',
+                'message' => 'Order ID tidak tersedia!',
+                'code'    => 422
+            ], 422);
+        }
+
+        $orderCanceled->status = 'Canceled';
+        $orderCanceled->save();
+
+        if($orderCanceled) {
+            return response()->json([
+                'success' => true,
+                'data'    => [
+                    'order'      => $orderCanceled
+                ],
+                'message' => 'Pesanan berhasil dibatalkan!',
+                'code'    => 200
+            ], 200);
+        }
+        
+        return response()->json([
+            'success' => false,
+            'data'    => '',
+            'message' => 'Pesanan tidak berhasil dibatalkan!',
+            'code'    => 406
+        ], 406);
+    }
 }
